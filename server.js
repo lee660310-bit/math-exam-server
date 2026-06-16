772e7d80be1b6966dc390aad577cab858c7b3f91
lee660310-bit <lee660310@gmail.com>
2026-06-16 22:59:43 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+{
+  "name": "math-exam-generator",
+  "version": "1.0.0",
+  "description": "리더스학원 수학 기출분석 & 문제 생성기 서버",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1",
+    "express-rate-limit": "^7.1.5",
+    "helmet": "^7.1.0",
+    "multer": "^1.4.5-lts.1",
+    "node-fetch": "^2.7.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
